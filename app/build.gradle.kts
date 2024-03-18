59a92cb4f859432671dd4205d9dd94049d56600e
Joshua Ostblom <joshua-ostblom@sandhills.com>
2024-03-18 09:05:02 -0500

    Update .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,6 @@ dependencies {
     // Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-compiler:2.50")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
 }
